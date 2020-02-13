df0b8ad8240b5d5e6aa74c526ccfb934160aaf12
捧花人 <37654847+OnePetrichor@users.noreply.github.com>
2020-02-13 13:30:27 +0800

    1.0

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# zhuanleApp
- 转乐APP程序源码
+# 转乐APP程序源码 `Version：1.0`
+## 使用教程
+> 使用
--- app.js
+++ app.js
@@ -6,25 +6,24 @@ App({
      * 当wap2app初始化完成时，会触发 onLaunch
      */
     onLaunch: function(options) {  
-		alert(plus.runtime.version);
+		//alert(plus.runtime.version);
         /************升级检测代码开始********** */  
         var ua = navigator.userAgent;  
         //Html5Plus环境，但不是流应用环境  
-        if(ua.indexOf('Html5Plus')>-1 && ua.indexOf('StreamApp')==-1){  
-            var url = "http://www.example.com/check/update";//检查更新地址  
+        if(ua.indexOf('Html5Plus')>-1 && ua.indexOf('StreamApp')==-1){
+            var url = "http://new.7demo.cn/admin/app/checkUpdate";//检查更新地址  
             var req = {//升级检测数据  
                 "appid": plus.runtime.appid,  
                 "version": plus.runtime.version  
-            };  
-    
-            wap2app.ajax.get(url, req, function(rsp) {  
+            };
+            wap2app.ajax.get(url, req, function(rsp) {
                 if(rsp && rsp.status){  
                     //需要更新，提示用户  
                     plus.nativeUI.confirm(rsp.note, function(event) {  
                         if(0 == event.index) {//用户点击了“立即更新”按钮  
                             plus.runtime.openURL(rsp.url);  
                         }  
-                    }, rsp.title, ["立即更新", "取　　消"]);  
+                    }, rsp.title, ["立即更新", "暂不更新"]);  
                 }              
             });  
         }  
